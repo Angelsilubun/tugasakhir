2dcd5cd52aefe24266c10ddf954acac4c7dd79cc
Angelsilubun <angelsilubun@gmail.com>
2023-07-13 12:02:56 +0700

    Done

--- resources/views/admin/basisrule/index_basis.blade.php
+++ resources/views/admin/basisrule/index_basis.blade.php
@@ -214,7 +214,7 @@
                 <div class="modal-dialog modal-lg">
                     <div class="modal-content">
                         <div class="modal-header">
-                            <h1 class="modal-title fs-5" id="exampleModalLabel">Edit Data Gejala</h1>
+                            <h1 class="modal-title fs-5" id="exampleModalLabel">Edit Basis Rules</h1>
                             <button type="button" class="btn-close" data-bs-dismiss="modal"
                                 aria-label="Close"></button>
                         </div>
--- resources/views/admin/petunjuk_penggunaan/index_petunjuk.blade.php
+++ resources/views/admin/petunjuk_penggunaan/index_petunjuk.blade.php
@@ -123,7 +123,7 @@
                 <div class="modal-dialog modal-lg">
                     <div class="modal-content">
                         <div class="modal-header">
-                            <h1 class="modal-title fs-5" id="exampleModalLabel">Tambah Data Tentang</h1>
+                            <h1 class="modal-title fs-5" id="exampleModalLabel">Tambah Data Petunjuk Penggunaan</h1>
                             <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                         </div>
                         <form class="needs-validation" action="{{ route('petunjuk_penggunaan.store') }}" method="POST"
